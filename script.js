d10107dd0f86f1991123ab94e8099f0506b7a9a8
Mukesh <mukeshdixena123@gmail.com>
2025-02-07 15:23:04 +0530

    create put api for edit

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 document.addEventListener("DOMContentLoaded", initApp);
 
+let editMode = false;
+let editId = null;
+
 async function initApp() {
     await fetchData();
     document.getElementById("myForm").addEventListener("submit", handleSubmit);
@@ -9,7 +12,6 @@ async function initApp() {
 async function fetchData() {
     try {
         const response = await axios.get("http://localhost:3000/api/getExpense");
-        console.log(response);
         response.data.forEach(({ id, description, amount }) => addToList(id, description, amount));
     } catch (error) {
         console.error("Error fetching data:", error);
@@ -23,15 +25,23 @@ async function handleSubmit(event) {
     const description = form.description.value.trim();
     const amount = form.amount.value.trim();
 
-
     if (!description || !amount) return alert("Please fill in all fields.");
 
     try {
-        const response = await axios.post("http://localhost:3000/api/postExpense", { description, amount });
-        addToList(response.data.id, description, amount);
+        if (editMode) {
+            await axios.put(`http://localhost:3000/api/editExpense/${editId}`, { description, amount });
+            // document.querySelector(`li[data-id='${editId}']`).remove();
+            addToList(editId, description, amount);
+            editMode = false;
+            editId = null;
+        } else {
+            const response = await axios.post("http://localhost:3000/api/postExpense", { description, amount });
+            addToList(response.data.id, description, amount);
+        }
+
         form.reset();
     } catch (error) {
-        console.error("Error posting data:", error);
+        console.error("Error submitting data:", error);
     }
 }
 
@@ -77,5 +87,7 @@ function editItem(li) {
 
     form.description.value = description;
     form.amount.value = amount;
+    editMode = true;
+    editId = id;
     li.remove();
 }
