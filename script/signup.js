38e5e007e35798c7c1e8feafa29810c2a566b490
Mukesh <mukeshdixena123@gmail.com>
2025-02-11 13:59:36 +0530

    Created Signup Screen Backend Apis

---
+++ script/signup.js
@@ -0,0 +1,27 @@
+async function signup() {
+    let username = document.getElementById('username').value;
+    let currEmail = document.getElementById('email').value;
+    let password = document.getElementById('password').value;
+
+    if (!username || !currEmail || !password) {
+        alert('All fields are required!');
+        return;
+    }
+
+    try {
+        const response = await axios.get("http://localhost:3000/api/getUser");
+
+        const emailExists = response.data.some(({ email }) => email === currEmail);
+
+        if (emailExists) {
+            alert('This email already exists. Please sign in.');
+            return;
+        }
+
+        await axios.post("http://localhost:3000/api/postUser", { username, email: currEmail, password });
+        alert('Signup successful!');
+    } catch (error) {
+        console.error('Error during signup:', error);
+        alert('An error occurred. Please try again later.');
+    }
+}
